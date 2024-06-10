fd265f2d87696bafca62a0f94b8990fd9adf75cc
Anderson Rodrigues <anderson.rodrigues@gruposolar.com.br>
2024-06-10 11:30:15 -0300

    Push

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,9 +1,7 @@
 import { useAuthContext } from "@/contexts/AuthContext";
-import moment from "moment";
 import React, { useEffect, useState } from 'react'
 import { IconContext } from "react-icons";
 import { AiFillLeftCircle, AiFillRightCircle } from "react-icons/ai";
-import { IoCopy } from "react-icons/io5";
 import ReactPaginate from "react-paginate";
 
 interface PaginationProps {
@@ -25,18 +23,18 @@ const Pagination = ({ data }: PaginationProps) => {
     return (
         <ReactPaginate
             containerClassName={"bg-gray-50 flex items-center justify-end gap-4 text-gray-400"}
-            pageClassName={"flex items-center justify-center hover:text-solar-blue-primary font-semibold"}
-            activeClassName={"bg-solar-blue-primary text-gray-50 hover:text-gray-50 rounded-full h-8 w-8"}
+            pageClassName={"flex items-center justify-center font-medium text-md"}
+            activeClassName={"bg-solar-blue-primary text-gray-50 rounded-full h-8 w-8"}
             onPageChange={(event) => setPage(event.selected)}
             pageCount={Math.ceil(data?.length / n)}
             breakLabel="..."
             previousLabel={
-                <IconContext.Provider value={{ color: "#B8C1CC", size: "36px" }}>
+                <IconContext.Provider value={{ color: "#B8C1CC", size: "34px" }}>
                     <AiFillLeftCircle />
                 </IconContext.Provider>
             }
             nextLabel={
-                <IconContext.Provider value={{ color: "#B8C1CC", size: "36px" }}>
+                <IconContext.Provider value={{ color: "#B8C1CC", size: "34px" }}>
                     <AiFillRightCircle />
                 </IconContext.Provider>
             }
